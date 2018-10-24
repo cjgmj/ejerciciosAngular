ccd81469edfb56935cea7a067011797b7ff7938d
cjgmj <cjgmj@hotmail.com>
2018-10-24 16:34:30 +0200

    slice

--- 03-pipes/src/app/app.component.ts
+++ 03-pipes/src/app/app.component.ts
@@ -6,6 +6,8 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  nombre: String = 'Juan';
+  nombre: String = 'Joaquín';
+
+  arr: number[] = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
 
 }
