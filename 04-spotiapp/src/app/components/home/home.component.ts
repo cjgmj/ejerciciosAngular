3287c8ab6cb74bf916b44859b810f82f891b12cf
cjgmj <cjgmj@hotmail.com>
2018-11-01 15:11:30 +0100

    Conexión con Spotify

--- 04-spotiapp/src/app/components/home/home.component.ts
+++ 04-spotiapp/src/app/components/home/home.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
+import { SpotifyService } from 'src/app/services/spotify.service';
 
 @Component({
   selector: 'app-home',
@@ -8,14 +8,8 @@ import { HttpClient } from '@angular/common/http';
 })
 export class HomeComponent implements OnInit {
 
-  countries: any[] = [];
-
-  constructor( private http: HttpClient ) {
-    this.http.get('https://restcountries.eu/rest/v2/lang/es')
-          .subscribe( (data: any[]) => {
-          this.countries = data;
-            console.log(data);
-          } );
+  constructor( private spotify: SpotifyService ) {
+    this.spotify.getNewReleases();
   }
 
   ngOnInit() {
---
+++ 04-spotiapp/src/app/services/spotify.service.ts
@@ -0,0 +1,21 @@
+import { Injectable } from '@angular/core';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class SpotifyService {
+
+  constructor( private http: HttpClient ) { }
+
+  getNewReleases() {
+
+    const headers = new HttpHeaders({
+      'Authorization': 'Bearer BQBYnRi2yYBsp9-hvbbpaodKtUqCpSTkyX0yLvH4eZls5OLgSYVPhmaYFCQqfN7WFdxW-Xg6EK1DfZQkt5A'
+    });
+
+    this.http.get('https://api.spotify.com/v1/browse/new-releases', { headers }).subscribe( data => {
+       console.log(data);
+    });
+  }
+}
