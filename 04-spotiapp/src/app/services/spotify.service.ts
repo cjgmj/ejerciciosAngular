23ab2053617fd08e659358180f3b4ca883c44cf2
cjgmj <cjgmj@hotmail.com>
2018-11-01 15:42:39 +0100

    Buscador de artistas

--- 04-spotiapp/src/app/services/spotify.service.ts
+++ 04-spotiapp/src/app/services/spotify.service.ts
@@ -15,5 +15,16 @@ export class SpotifyService {
     });
 
     return this.http.get('https://api.spotify.com/v1/browse/new-releases', { headers });
+
+  }
+
+  getArtist (artist: string) {
+
+    const headers = new HttpHeaders({
+      'Authorization': 'Bearer BQBYnRi2yYBsp9-hvbbpaodKtUqCpSTkyX0yLvH4eZls5OLgSYVPhmaYFCQqfN7WFdxW-Xg6EK1DfZQkt5A'
+    });
+
+    return this.http.get(`https://api.spotify.com/v1/search?q=${ artist }&type=artist&limit=15`, { headers });
+
   }
 }
