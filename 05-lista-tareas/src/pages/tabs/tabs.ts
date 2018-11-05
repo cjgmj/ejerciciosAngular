aa41fff0132af6e3b478ac292537d7d747c353ec
cjgmj <cjgmj@hotmail.com>
2018-11-05 16:55:09 +0100

    Estructura base

--- 05-lista-tareas/src/pages/tabs/tabs.ts
+++ 05-lista-tareas/src/pages/tabs/tabs.ts
@@ -1,17 +1,15 @@
 import { Component } from '@angular/core';
+import { PendingPage } from '../pending/pending.component';
+import { FinishedPage } from '../finished/finished.component';
 
-import { AboutPage } from '../about/about';
-import { ContactPage } from '../contact/contact';
-import { HomePage } from '../home/home';
 
 @Component({
   templateUrl: 'tabs.html'
 })
 export class TabsPage {
 
-  tab1Root = HomePage;
-  tab2Root = AboutPage;
-  tab3Root = ContactPage;
+  tabPending = PendingPage;
+  tabFinished = FinishedPage;
 
   constructor() {
 
