5bacb2a563f66e10dfd4e2a1731d43b164b4db73
cjgmj <cjgmj@hotmail.com>
2018-11-16 23:18:18 +0100

    Validaciones y cambio de estilos

--- 08-formularios/src/app/components/template/template.component.ts
+++ 08-formularios/src/app/components/template/template.component.ts
@@ -4,14 +4,16 @@ import { NgForm } from '@angular/forms';
 @Component({
   selector: 'app-template',
   templateUrl: './template.component.html',
-  styles: []
+  styles: [`.ng-invalid.ng-touched:not(form) {
+    border: 1px solid red;
+  }`]
 })
 export class TemplateComponent {
 
   usuario: Object = {
-    nombre: 'Ibai',
-    apellido: 'Gómez',
-    email: 'ibai.gomez@yopmail.com'
+    nombre: null,
+    apellido: null,
+    email: null
   };
 
   constructor() { }
