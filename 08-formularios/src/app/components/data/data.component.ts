bc5184563b66e7c1e2848c17f3db23bf917167ca
cjgmj <cjgmj@hotmail.com>
2018-11-17 12:44:54 +0100

    Validaciones

--- 08-formularios/src/app/components/data/data.component.ts
+++ 08-formularios/src/app/components/data/data.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { FormGroup, FormControl } from '@angular/forms';
+import { FormGroup, FormControl, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-data',
@@ -12,14 +12,14 @@ export class DataComponent {
 
   constructor() {
     this.formulario = new FormGroup({
-      'nombre': new FormControl(),
-      'apellido': new FormControl(),
-      'correo': new FormControl()
+      'nombre': new FormControl('', [Validators.required, Validators.minLength(3)]),
+      'apellido': new FormControl('', Validators.required),
+      'email': new FormControl('', [Validators.required, Validators.pattern('[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,3}$')])
     });
   }
 
   guardarCambios() {
-    
+
   }
 
 }
