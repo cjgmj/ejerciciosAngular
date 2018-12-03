6b3482b95ce2f24f35c236ee302d4eb1ea4548f9
cjgmj <cjgmj@hotmail.com>
2018-12-03 17:56:29 +0100

    Uso marcadores

--- 12-mapas/src/app/components/mapa/mapa.component.ts
+++ 12-mapas/src/app/components/mapa/mapa.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { MatSnackBar } from '@angular/material';
 import { Marcador } from '../../classes/marcador.class';
 
 @Component({
@@ -13,8 +14,10 @@ export class MapaComponent implements OnInit {
   lat = 51.678418;
   lng = 7.809007;
 
-  constructor() {
-    this.marcadores.push(new Marcador(51.678418, 7.809007));
+  constructor( public snackBar: MatSnackBar ) {
+    if ( localStorage.getItem('marcadores')) {
+      this.marcadores = JSON.parse(localStorage.getItem('marcadores'));
+    }
   }
 
   ngOnInit() {
@@ -23,6 +26,18 @@ export class MapaComponent implements OnInit {
   agregarMarcador( event ) {
     const coords: { lat: number, lng: number } = event.coords;
     this.marcadores.push(new Marcador(coords.lat, coords.lng));
+    this.guardarStorage();
+    this.snackBar.open('Marcador agregado', 'Cerrar', { duration: 3000 });
+  }
+
+  eliminarMarcador( index: number ) {
+    this.marcadores.splice(index, 1);
+    this.guardarStorage();
+    this.snackBar.open('Marcador borrado', 'Cerrar', { duration: 3000 });
+  }
+
+  guardarStorage() {
+    localStorage.setItem('marcadores', JSON.stringify(this.marcadores));
   }
 
 }
