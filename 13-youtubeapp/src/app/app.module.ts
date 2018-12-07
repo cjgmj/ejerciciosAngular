c63ea3acabcc77efb6c3c7215f06b82ad2a859e8
cjgmj <cjgmj@hotmail.com>
2018-12-07 20:39:44 +0100

    Interfaz del canal

--- 13-youtubeapp/src/app/app.module.ts
+++ 13-youtubeapp/src/app/app.module.ts
@@ -2,13 +2,19 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { HomeComponent } from './components/home/home.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { HttpModule } from '@angular/http';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    NavbarComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    HttpModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ 13-youtubeapp/src/app/services/youtube.service.ts
@@ -0,0 +1,38 @@
+import { Injectable } from '@angular/core';
+import { Http, URLSearchParams } from '@angular/http';
+import { map } from 'rxjs/operators';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class YoutubeService {
+
+  youtubeUrl = 'https://www.googleapis.com/youtube/v3';
+  apiKey = 'AIzaSyDuT6_uC6gTec0MZjYbPc_-VAQ6SQikVdA';
+  playList = 'UUa3DVlGH2_QhvwuWlPa6MDQ';
+
+  private nextPageToken = '';
+
+  constructor( private http: Http ) { }
+
+  getVideos() {
+    const url = `${ this.youtubeUrl }/playlistItems`;
+
+    const params = new URLSearchParams();
+    params.set('part', 'snippet');
+    params.set('maxResults', '10');
+    params.set('playlistId', this.playList);
+    params.set('key', this.apiKey);
+
+    return this.http.get(url, { search: params }).pipe(map( res => {
+      this.nextPageToken = res.json().nextPageToken;
+      const videos: any[] = [];
+
+      for (const video of res.json().items) {
+        videos.push(video.snippet);
+      }
+
+      return videos;
+    }));
+  }
+}
