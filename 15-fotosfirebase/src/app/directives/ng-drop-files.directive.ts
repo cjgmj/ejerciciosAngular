846c2f055e41cabe13e3f5e634081d0da319992c
cjgmj <cjgmj@hotmail.com>
2018-12-10 18:03:11 +0100

    Validaciones desde la directiva

--- 15-fotosfirebase/src/app/directives/ng-drop-files.directive.ts
+++ 15-fotosfirebase/src/app/directives/ng-drop-files.directive.ts
@@ -1,10 +1,12 @@
-import { Directive, HostListener, Output, EventEmitter } from '@angular/core';
+import { Directive, HostListener, Output, EventEmitter, Input } from '@angular/core';
+import { FileItem } from '../models/file-item';
 
 @Directive({
   selector: '[appNgDropFiles]'
 })
 export class NgDropFilesDirective {
 
+  @Input() archivos: FileItem[] = [];
   @Output() mouseSobre: EventEmitter<boolean> = new EventEmitter();
 
   constructor() { }
@@ -17,6 +19,32 @@ export class NgDropFilesDirective {
   @HostListener('dragleave', ['$event'])
   public onDragLeave( event: any ) {
     this.mouseSobre.emit(false);
+  }
+
+  // Validaciones
+  private _archivoPuedeSerCargado( archivo: File ): boolean {
+    if (!this._archivoYaExiste( archivo.name ) && this.esImagen ( archivo.type )) {
+      return true;
+    }
+    return false;
+  }
+
+  private _prevenirYDetener( event ) {
+    event.preventDefault();
+    event.stopPropagation();
+  }
+
+  private _archivoYaExiste( nombreArchivo: string ): boolean {
+    for ( const archivo of this.archivos ) {
+      if (archivo.nombreArchivo === nombreArchivo ) {
+        return true;
+      }
+    }
+    return false;
+  }
+
+  private esImagen( tipoArchivo: string ): boolean {
+    return ( tipoArchivo === '' || tipoArchivo === undefined) ? false : tipoArchivo.startsWith('image');
   }
 
 }
