f1f2ede0a4bf9e6d705ba365dd89a014892184e6
cjgmj <cjgmj@hotmail.com>
2018-12-10 17:14:06 +0100

    Componentes y rutas

--- 15-fotosfirebase/src/app/app.module.ts
+++ 15-fotosfirebase/src/app/app.module.ts
@@ -2,13 +2,20 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { FotosComponent } from './components/fotos/fotos.component';
+import { CargaComponent } from './components/carga/carga.component';
+
+import { APP_ROUTING } from './app.routes';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    FotosComponent,
+    CargaComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    APP_ROUTING
   ],
   providers: [],
   bootstrap: [AppComponent]
