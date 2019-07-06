eee94a52ea2565ba6abfcf430b1142c5a05862a6
cjgmj <cjgmj@hotmail.com>
2019-07-06 14:06:28 +0200

    Servicio REST

---
+++ 07-bouns-loginApp/src/app/services/auth.service.ts
@@ -0,0 +1,32 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { UsuarioModel } from '../models/usuario.model';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AuthService {
+
+  private url = 'https://www.googleapis.com/identitytoolkit/v3/relyingparty';
+  private apikey = 'AIzaSyAai9Jqdp00LPmdKo9xHMIC_OEnjhrCb1s';
+
+  // Crear nuevo usuario
+  // https://www.googleapis.com/identitytoolkit/v3/relyingparty/signupNewUser?key=[API_KEY]
+
+  // Login
+  // https://www.googleapis.com/identitytoolkit/v3/relyingparty/verifyPassword?key=[API_KEY]
+
+  constructor( private http: HttpClient ) { }
+
+  logout() {
+
+  }
+
+  login( usuario: UsuarioModel ) {
+
+  }
+
+  nuevoUsuario( usuario: UsuarioModel ) {
+
+  }
+}
